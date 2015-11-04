6a6a9eca2a951cf6e856aafb1812f33f06402990
Lam Nguyen <lam@mannapovllc.com>
2015-11-04 13:32:56 -0600

    Allow articles and comments use current_user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ActiveRecord::Base
   extend FriendlyId
   include PublicActivity::Model
-  tracked
+  tracked owner: :current_user
 
   friendly_id :title, use: :slugged
+  cattr_accessor :current_user
 
   belongs_to :user
   has_many :comments
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   include PublicActivity::Model
-  tracked
+  tracked owner: :current_user, recipient: :article
+  cattr_accessor :current_user
   
   belongs_to :user
   belongs_to :article
