9b5dd9c80c45b5e73627bdd4813e15a777dbc1f8
Lam Nguyen <lam@mannapovllc.com>
2015-11-04 08:38:17 -0600

    show recent activities on user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   def show
-      @articles = @user.articles
+      @articles   = @user.articles      
+      @activities = PublicActivity::Activity.order("created_at DESC").where(owner_id: current_user, owner_type: "User")
   end
 
   private
     def set_user
       @user = User.friendly.find(params[:id])
     end
+
 end
