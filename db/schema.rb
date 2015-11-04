b7aaa368b71d56afbe7d1e25c1a8e79a84ccf608
Lam Nguyen <me@lamguy.com>
2015-11-03 21:54:29 -0600

    added username to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104022319) do
+ActiveRecord::Schema.define(version: 20151104034212) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -110,10 +110,12 @@ ActiveRecord::Schema.define(version: 20151104022319) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
